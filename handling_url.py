c7fd6f9d6e01dd801403b5b9ecb2680c80a8f48f
Cotidie <51331195+Cotidie@users.noreply.github.com>
2020-07-18 12:24:15 +0900

    Add files via upload

--- handling_url.py
+++ handling_url.py
@@ -1,5 +1,14 @@
 from short_url import encode_url
+from urllib.parse import urlparse
 
 def create_short_url(unique_id:int):
     # 새로운 URL을 생성한다.
     return encode_url(unique_id, min_length=7)
+
+def validate_url(raw_url):
+    # 올바른 URL 형식인지 확인한다.
+    try:
+        urlparse(raw_url)
+        return True
+    except:
+        return False
