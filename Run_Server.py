8600a47cc421d653dee80b2b013f78cc1e30f486
Cotidie <51331195+Cotidie@users.noreply.github.com>
2020-07-20 23:13:26 +0900

    Add files via upload

--- Run_Server.py
+++ Run_Server.py
@@ -1,5 +1,5 @@
 from handling_db import MongoDB
-from handling_url import make_it_short
+from handling_url import create_short_url, validate_url
 from flask import Flask, render_template, url_for, request, redirect
 app = Flask(__name__)
 Mongo = MongoDB()
@@ -14,27 +14,47 @@ def home():
 def shorten_url():
     # AJAX를 통해 원본 URL을 처리하는 페이지.
 
-    # URL을 축약하는 부분
-    raw_url = request.get_data()
-    short_url = make_it_short(raw_url)
+    # 원본 URL 데이터 가져오기
+    raw_url = request.form['raw_url']
+    if not validate_url(raw_url):
+        return "Invalid URL"
 
-    # DB에 넣는 부분
-    db_data = {
-        'Raw_URL': raw_url,
-        'Short_URL': short_url
-    }
-    Mongo.save_data(db_data)
+    # 이미 저장된 URL인지 확인
+    short_url = Mongo.get_short_url(raw_url)
+
+    # 새로운 URL 일경우
+    if short_url is None:
+        print("raw_url is not in db")
+
+        # 짧은 URL 생성
+        unique_id = Mongo.get_unique_id()
+        short_url = create_short_url(unique_id)
+
+        # DB에 추가
+        db_data = {
+            'Raw_URL': raw_url,
+            'Short_URL': short_url
+        }
+        Mongo.save_data(db_data)
+
+        # DB 추가 후 unique_id를 증가시킨다.
+        Mongo.increase_id()
+
+        print("raw_url: ", raw_url, " short_url: ", short_url)
 
     # AJAX에 축약된 URL을 반환한다.
-    return short_url
+    return request.host_url + short_url
 
 @app.route('/<short_url>')
 def redirect_url(short_url):
     # 축약된 URL이 들어오면 DB에서 찾아 원본 링크로 연결한다.
-    Raw_URL = Mongo.get_URL({'Short_URL': short_url})     # 샘플 코드, 확인 후 삭제할 것.
+    Raw_URL = Mongo.get_raw_url(short_url)
 
-    return redirect(Raw_URL)                              # 해당 URL로 이동한다.
+    # 페이지가 존재하지 않으면 오류 페이지 출력
+    if Raw_URL is None:
+        return render_template('page_404.html')
 
+    return redirect(Raw_URL)
 
 if __name__ == "__main__":
     app.run(debug=True)
