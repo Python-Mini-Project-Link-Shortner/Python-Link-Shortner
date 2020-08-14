8af3fd1aae376f00762d0d3efad170ce1a5df1d5
Cotidie <51331195+Cotidie@users.noreply.github.com>
2020-08-14 09:56:40 +0900

    Fixed an error
    an error pertaining to the attachment between v-text-field and v-btn. solved by using 'append' slot of v-text-field.

--- frontend/src/components/Main/Content/Jumbotron.vue
+++ frontend/src/components/Main/Content/Jumbotron.vue
@@ -1,15 +1,42 @@
 <template>
   <v-parallax dark :src="imgPath" height="700">
     <v-container style="max-width: 800px;">
-        <v-row align="center">
+        <v-row align="center" justify="center" no-gutters>
             <v-col class="text-center" cols="12">
                 <v-text-field
                 v-model="url"
                 solo
                 clearable
                 rounded
+                autofocus
                 placeholder="Input Your Link Here."
-                />
+                >
+                    <template slot="append">
+                        <v-btn color="primary">Shorten</v-btn>
+                    </template>
+                </v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="10">
+                <v-alert 
+                  class="ma-0" 
+                  type="success" 
+                  dense 
+                  dark 
+                  dismissible
+                  >
+                  성공!
+                </v-alert>
+
+                <v-alert 
+                  class="ma-0" 
+                  type="warning" 
+                  dense 
+                  dark 
+                  dismissible
+                  >
+                  실패!
+                </v-alert>
             </v-col>
         </v-row>  
     </v-container>
@@ -22,15 +49,12 @@ export default {
     data() {
         return {
             imgPath: require('@/assets/img/jumbo 2.jpg'),
-            url: ''
+            url: '',
+            isSuccess: false
         }
     },
-    mounted() {
-        console.log(this.$vuetify.application.top)
-    }
 }
 </script>
 
-<style>
-
+<style scoped>
 </style>
