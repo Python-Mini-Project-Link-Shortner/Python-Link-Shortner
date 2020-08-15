5faa401baeb544822a34165bc0641a233107dec8
Cotidie <51331195+Cotidie@users.noreply.github.com>
2020-08-15 09:57:07 +0900

    Added some comments

--- frontend/src/components/Main/AppContent.vue
+++ frontend/src/components/Main/AppContent.vue
@@ -1,15 +1,18 @@
 <template>
   <v-main>
       <Jumbotron />
+
+      <MainFeatures />
   </v-main>
 </template>
 
 <script>
 import Jumbotron from './Content/Jumbotron.vue'
+import MainFeatures from './Content/MainFeatures.vue'
 export default {
     name: 'AppContent',
     components: {
-        Jumbotron
+        Jumbotron, MainFeatures
     }
 }
 </script>
