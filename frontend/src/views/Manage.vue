dd7c9f669d40c0ac74f78957594248e5a9bb2923
cjcjon <cjcjon@naver.com>
2020-08-30 14:59:55 +0900

    CSS 변경
    CSS 깔끔하게 수정함
    TODO에 맞춰서 추가수정 필요

--- frontend/src/views/Manage.vue
+++ frontend/src/views/Manage.vue
@@ -1,54 +1,215 @@
 <template>
   <v-container class="text-left" px-6 py-5 fluid>
+    <!-- 분석창 -->
     <v-row>
       <v-col cols="12">
-        <div style="height: 240px; background-color: cyan; border-radius: 8px;">여러가지 분석자료들 넣을 수 있으면 넣어보세요</div>
+        <div style="height: 240px; border: 1px solid black; border-radius: 8px;">여러가지 분석자료들 넣을 수 있으면 넣어보세요</div>
       </v-col>
     </v-row>
 
+    <!-- Link Shortner 입력창 -->
     <v-row>
       <v-col cols="12">
         <LinkInput alert></LinkInput>
       </v-col>
     </v-row>
 
+    <!-- 링크 -->
     <v-row>
-      <v-col cols="12" class="text-right">
-        <v-btn class="mx-2" color="primary" :disabled="favoriteDisable">관심등록</v-btn>
-        <v-btn class="mx-2" color="error" :disabled="removeDisable">삭제</v-btn>
-        <v-btn class="mx-2" color="success" :disabled="tagDisable">#태그</v-btn>
-        <v-btn class="mx-2" color="secondary" :disabled="hideDisable">숨기기</v-btn>
-      </v-col>
-
       <v-col cols="12" v-for="n in 10" :key="n">
-        <v-card :elevation="3">
-          <v-list-item class="grey lighten-2 height-48">
-            <div class="text-truncate font-weight-bold">탭이름 (길면...)</div>
+        <v-card outlined hover>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6 text-truncate" style="color: #E71D36;">minipy.shorten.at/abcdefg</v-list-item-title>
+              <v-list-item-subtitle>2020-08-28</v-list-item-subtitle>
+            </v-list-item-content>
             <v-spacer></v-spacer>
-            <div class="grey--text lighten-1 pr-4 ">2020-08-13</div>
-            <v-checkbox v-model="checkboxTest[n - 1]"></v-checkbox>
+            <v-checkbox v-model="clickedLink" :value="n"></v-checkbox>
           </v-list-item>
-          <v-divider class="grey lighten-1"></v-divider>
 
-          <v-card-text>
-            <div class="text-h6 red--text text--darken-1 pb-1">pysh.com/ABC</div>
-            <div class="text-truncate">http://naver.com</div>
-          </v-card-text>
+          <v-card-subtitle class="py-0 font-weight-regular">네이버</v-card-subtitle>
+          <v-card-text class="text--primary pb-1">https://www.naver.com</v-card-text>
 
-          <v-card-actions class="px-3 pt-0">
-            <v-btn outlined color="primary">복사</v-btn>
-            <v-btn outlined color="error">제거</v-btn>
-            <v-btn outlined color="secondary">탭이름 변경</v-btn>
-            <v-btn outlined color="success">#태그</v-btn>
-            <v-btn outlined color="warning">기타</v-btn>
+          <v-card-actions class="px-4">
+            <div><span class="info--text">#Empty</span></div>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon dark color="success" @click.stop="favoriteDialog = true;">
+              <v-icon>mdi-heart-outline</v-icon>
+            </v-btn>
+            <v-btn icon dark color="info" @click.stop="tagDialog = true;">
+              <v-icon>mdi-tag-outline</v-icon>
+            </v-btn>
+            <v-btn icon dark color="warning" @click.stop="hideDialog = true;">
+              <v-icon>mdi-eye-off-outline</v-icon>
+            </v-btn>
+            <v-btn icon dark color="error" @click.stop="deleteDialog = true;">
+              <v-icon>mdi-delete-outline</v-icon>
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
 
+      <!-- 페이지네이션 -->
       <v-col cols="12">
         <v-pagination v-model="page" prev-icon="mdi-menu-left" next-icon="mdi-menu-right" :length="10" :total-visible="10"></v-pagination>
       </v-col>
     </v-row>
+
+    <!-- Floating Button -->
+    <v-speed-dial v-model="fab" fixed right bottom :transition="'slide-y-reverse-transition'">
+      <template v-slot:activator>
+        <v-btn v-model="fab" fab dark color="pink darken-1">
+          <v-icon v-if="fab">mdi-close</v-icon>
+          <v-icon v-else>mdi-plus</v-icon>
+        </v-btn>
+      </template>
+      <v-btn fab dark small color="error" :disabled="fabDisabled" @click.stop="deleteDialog = true;">
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
+      <v-btn fab dark small color="warning" :disabled="fabDisabled" @click.stop="hideDialog = true;">
+        <v-icon>mdi-eye-off</v-icon>
+      </v-btn>
+      <v-btn fab dark small color="info" :disabled="fabDisabled" @click.stop="tagDialog = true;">
+        <v-icon>mdi-tag</v-icon>
+      </v-btn>
+      <v-btn fab dark small color="success" :disabled="fabDisabled" @click.stop="favoriteDialog = true;">
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+    </v-speed-dial>
+
+    <v-dialog v-model="deleteDialog" max-width="360">
+      <v-card>
+        <v-card-title class="headline error--text">
+          링크 삭제
+        </v-card-title>
+        <v-card-text class="pb-0">
+          해당 작업은 되돌릴 수 없습니다.<br/>
+          정말로 삭제하시겠습니까?
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="error" text class="font-weight-bold" @click="deleteDialog = false;">
+            삭제
+          </v-btn>
+          <v-btn color="warning" text class="font-weight-bold" @click="deleteDialog = false;">
+            취소
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- 숨김 Dialog -->
+    <v-dialog v-model="hideDialog" max-width="360">
+      <v-card>
+        <v-card-title class="headline">
+          링크 숨기기
+        </v-card-title>
+        <v-card-text class="pb-0">
+          숨길 위치를 선택하세요<br/>
+          선택 Menu나오기
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="info" text class="font-weight-bold" @click="hideDialog = false;">
+            위치 추가
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="success" text class="font-weight-bold" @click="hideDialog = false;">
+            숨기기
+          </v-btn>
+          <v-btn color="error" text class="font-weight-bold" @click="hideDialog = false;">
+            취소
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- 태그추가 Dialog -->
+    <v-dialog v-model="tagDialog" max-width="360">
+      <v-card>
+        <v-card-title class="headline">
+          태그
+          <v-spacer></v-spacer>
+          <v-btn color="error" icon class="font-weight-bold" @click="tagDialog = false;">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text class="pb-0">
+          <v-row>
+            <v-col cols="6">
+              <v-btn color="success" dark class="full-width" @click="tagChangeDialog = true;">
+                태그 변경
+              </v-btn>
+            </v-col>
+            <v-col cols="6">
+              <v-btn color="error" dark class="full-width" @click="tagDeleteDialog = true;">
+                태그 삭제
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
+    <!-- 태그 변경 Dialog -->
+    <v-dialog v-model="tagChangeDialog" max-width="360">
+      <v-card>
+        <v-card-title class="headline">태그 변경</v-card-title>
+        <v-card-text class="pb-0">
+          <v-text-field label="#태그" v-model="tagName"></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="success" text class="font-weight-bold" @click="tagChangeDialog = false;">
+            변경
+          </v-btn>
+          <v-btn color="error" text class="font-weight-bold" @click="tagChangeDialog = false;">
+            취소
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- 태그 삭제 Dialog -->
+    <v-dialog v-model="tagDeleteDialog" max-width="320">
+      <v-card class="py-2">
+        <v-card-text class="pb-0">
+          <v-container class="pb-0 text-subtitle-1 font-weight-medium black--text">
+            해당 작업은 되돌릴 수 없습니다.<br/>
+            태그를 지우시겠습니까?
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="success" text class="font-weight-bold" @click="tagDeleteDialog = false;">
+            확인
+          </v-btn>
+          <v-btn color="error" text class="font-weight-bold" @click="tagDeleteDialog = false;">
+            취소
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- 즐겨찾기 Dialog -->
+    <v-dialog v-model="favoriteDialog" width="360">
+      <v-card>
+        <v-card-title class="headline">즐겨찾기 추가</v-card-title>
+        <v-card-text>
+          ~~~~~~~~~~ 을 즐겨찾기에 추가하시겠습니까?
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="success" text class="font-weight-bold" @click="favoriteDialog = false;">
+            확인
+          </v-btn>
+          <v-btn color="error" text class="font-weight-bold" @click="favoriteDialog = false;">
+            취소
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -61,20 +222,27 @@ export default {
     LinkInput: ShortenLink
   },
   data: () => ({
-    page: 1,
-    favoriteDisable: true,
-    removeDisable: true,
-    tagDisable: true,
-    hideDisable: true,
-    checkboxTest: []
+    page: 1, // 현재 페이지 번호
+    fab: false, // Floating Button 클릭 상태
+    clickedLink: [], // 현재 클릭한 링크 아이디 배열
+    tagName: '', // 변경할 태그 이름
+    deleteDialog: false, // 삭제 dialog
+    hideDialog: false, // 숨김 dialog
+    tagDialog: false, // 태그 dialog
+    tagChangeDialog: false, // 태그 변경 dialog
+    tagDeleteDialog: false, // 태그 삭제 dialog
+    favoriteDialog: false // 즐겨찾기 dialog
   }),
-  methods: {
+  computed: {
+    fabDisabled: function() {
+      return this.clickedLink.length === 0;
+    }
   }
 }
 </script>
 
 <style scoped>
-  .height-48 {
-    height: 48px !important;
-  }
+.full-width {
+  width: 100%;
+}
 </style>
