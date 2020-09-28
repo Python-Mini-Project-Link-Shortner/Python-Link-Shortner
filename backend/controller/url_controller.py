9098fb56a4fbf06bafe48c5d4c0a497e8caec9d0
Cotidie <51331195+Cotidie@users.noreply.github.com>
2020-09-28 21:06:14 +0900

    url_service 수정
    shorten 페이지 완성
    
    TODO: check, redirect 페이지

--- backend/controller/url_controller.py
+++ backend/controller/url_controller.py
@@ -3,7 +3,7 @@ from backend.service    import url_service
 
 url_controller = Blueprint('url_controller', __name__)
 
-@app.route('shorten', methods=['GET', 'POST'])
+@url_controller.route('shorten', methods=['GET', 'POST'])
 def shorten_url():
     # 원본 URL 데이터 가져오기
     req_data = request.get_json()
@@ -18,44 +18,22 @@ def shorten_url():
         res_data['msg'] = 'Invalid URL'
         return jsonify(res_data)
 
-    # 프로토콜이 생략된 url을 수정한다.
-    raw_url = normalize_url(raw_url)
+    # 축약 URL을 등록한다.
+    short_url = url_service.register_url(raw_url)
+    if short_url == False:
+        # 저장에 실패한 경우
+        res_data['flag'] = False
+        res_data['msg']  = 'DB save failed'
+        return jsonify(res_data)
 
-    # 이미 저장된 URL인지 확인
-    short_url = Mongo.get_short_url(raw_url)
+    print("raw_url: ", raw_url, " short_url: ", short_url)
 
-    # 새로운 URL 일경우
-    if short_url is None:
-        # 짧은 URL 생성
-        unique_id = Mongo.get_unique_id()
-        short_url = create_short_url(unique_id)
-
-        # DB에 추가
-        db_data = {
-            'rawURL': raw_url,
-            'shortURL': short_url
-        }
-        flag = Mongo.save_data(db_data, 'LINKS')
-
-        # DB 추가 후 unique_id를 증가시킨다.
-        if flag:
-            Mongo.increase_id()
-        else:
-            return jsonify({
-                'flag': False,
-                'msg': 'DB save failed.'
-            })
-
-        print("raw_url: ", raw_url, " short_url: ", short_url)
-
-    # 축약된 URL을 반환한다.
-    return jsonify({
-        'flag': True,
-        'msg': request.host_url + short_url
-        })
+    res_data['flag'] = True
+    res_data['msg']  = request.host_url + short_url
+    return jsonify(res_data)
 
 # 축약된 URL의 원본 URL을 반환하는 페이지
-@app.route('/api/check', methods=['GET', 'POST'])
+@url_controller.route('/api/check', methods=['GET', 'POST'])
 def check_url():
     short_url = request.json['url']
     
--- backend/service/url_service.py
+++ backend/service/url_service.py
@@ -1,36 +1,145 @@
-from short_url import encode_url
-from urllib.parse import urlparse
+from short_url                  import encode_url
+from urllib.parse               import urlparse
+from backend.database.mongo_db  import db
 
-# 새로운 URL을 생성한다.
 def create_short_url(unique_id:int):
+    """7자리 문자열을 생성한다.
+    
+    Args:
+        unique_id (int): 고유 숫자
+    Returns:
+        str : 고유 숫자에 대응하는 7자리 문자열
+    """
     return encode_url(unique_id, min_length=7)
     
-# 올바른 URL 형식인지 확인한다.
 def validate_url(raw_url):
+    """올바른 URL 형식인지 확인한다.
+
+    Args:
+        raw_url (str): 검사할 URL
+    Returns:
+        bool: 올바른 URL이면 True, 아니면 False
+    """
     try:
         urlparse(raw_url)
         return True
     except:
         return False
 
-# 프로토콜이 생략된 url을 수정한다.
 def normalize_url(raw_url):
-    # URL에 프로토콜이 생략된 경우 추가한다.
+    """URL에 생략된 부분이 있으면 붙여준다.
+
+    Args:
+        raw_url (str): 수정할 원본 URL
+
+    Returns:
+        str: 수정된 URL
+    """
+    # 프로토콜을 추가한다.
     # 그대로 파싱할 경우 도메인을 path로 인식한다.
     valid_protocols = ('http://', 'https://', '//')
     if not raw_url.startswith(valid_protocols):
         raw_url = '//' + raw_url
 
-    # URL을 파싱한다. 프로토콜이 명시되지 않은 경우 http로 파싱.
+    # URL을 파싱한다. 기본 프로토콜은 http.
     parse = urlparse(raw_url, scheme='http')
 
-    # 전체 url 주소를 반환한다.
     return parse.geturl()
 
-def get_short_url(raw_url):
-    """
+def get_url(url, target="short"):
+    """DB에서 축약된 URL을 가져온다.
+
     Args:
-        raw_url: 원본 링크
+        url (str): 원본 URL
+        target (str): 검색할 URL (short | raw)
+
     Returns:
-        
+        str: 축약 URL
+        None: DB에 없는 경우
+
+    Raises:
+        ValueError: target 인수값이 잘못된 경우
     """
+    target = target.lower()
+    if target not in ('short', 'raw'):
+        raise ValueError('target 인수가 잘못되었습니다. short 또는 raw로 입력해주십시오.')
+
+    collection = db.get_collection()
+
+    if target == 'short':
+        search_from = 'rawURL'
+        search_for  = 'shortURL'
+    elif target == 'raw':
+        search_from = 'shortURL'
+        search_for  = 'rawURL'
+
+    res = collection.find({ search_from: url })
+
+    if res is None: return None
+    return res[search_for]
+
+def get_unique_id():
+    """URL 생성을 위한 고유 ID값을 가져온다.
+
+    Returns:
+        int: 고유 ID값
+        None: 찾지 못한 경우
+    """
+    collection = db.get_collection('CONFIG')
+    
+    my_query    = {'variable': 'unique_id'}     # 검색필드
+    my_result   = {'value': True}               # 결과필드
+
+    res = collection.find_one(my_query, my_result)
+
+    return res['value']
+
+def increase_id():
+    """URL 생성에 사용될 ID(int)를 증가시킨다.
+
+    Returns:
+        bool: 성공/실패 = True/False
+    """
+    collection = db.get_collection('CONFIG')
+
+    my_query    = {'variable': 'unique_id'}   # variable 필드값이 unique_id
+    new_value   = {'$inc': {'value': 1}}      # value 필드를 1 증가
+    
+    res = collection.update_one(my_query, new_value)
+
+    return res.modified_count > 0        
+
+def register_url(raw_url):
+    """축약 링크를 등록한다.
+
+    Args:
+        short_url (str): 축약 URL
+        raw_url (str): 원본 URL
+
+    Returns:
+        str: DB에 등록된 축약 URL을 반환한다.
+    """
+    collection = db.get_collection()
+
+    raw_url     = normalize_url(raw_url)
+    short_url   = get_url(raw_url, target="short")
+
+    # 미등록인 경우 새로 등록
+    if short_url is None:
+        unique_id = get_unique_id()
+        short_url = create_short_url(unique_id)
+
+        # DB에 신규등록
+        data = {
+            'rawURL': raw_url,
+            'shortURL': short_url
+        }
+        try:
+            collection.insert_one(data)
+        except Exception as e:
+            # 저장에 실패한 경우 False 반환
+            print(e)
+            return False
+        increase_id()
+
+    return short_url
