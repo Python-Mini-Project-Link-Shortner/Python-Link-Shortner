133c1f4549f838d5ede666a147d1bf35b9b21444
Cotidie <51331195+Cotidie@users.noreply.github.com>
2020-10-18 00:16:29 +0900

    Google Oauth

--- backend/service/login_service.py
+++ backend/service/login_service.py
@@ -5,6 +5,10 @@ from oauth2client       import client
 
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', None)
 GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET', None)
+if GOOGLE_CLIENT_ID is None:
+    raise Exception("환경변수 GOOGLE_CLIENT_ID를 설정해주십시오.")
+if GOOGLE_CLIENT_SECRET is None:
+    raise Exception("환경변수 GOOGLE_CLIENT_SECRET을 설정해주십시오.")
 
 def handshake_oauth(code):
 
@@ -17,7 +21,8 @@ def handshake_oauth(code):
     )
 
     # 이메일과 아이디 받기
-    user_id = credentials.id_token['sub']
-    email = credentials.id_token['email']
+    print('Acess Token: ', credentials.access_token)
+    print('Refresh Token: ', credentials.refresh_token)
+    print('ID Token: ', credentials.id_token)
 
     # TODO: refresh 토큰 저장, 결과값 반환, frontend >> account.js 완성
