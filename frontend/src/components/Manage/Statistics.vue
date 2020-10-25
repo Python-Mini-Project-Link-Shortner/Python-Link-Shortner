38811cc6e96246e8b3e599b9e06e7e3534764efd
Cotidie <51331195+Cotidie@users.noreply.github.com>
2020-10-26 00:10:42 +0900

    통계 페이지용 서브헤더

--- frontend/src/components/Manage/Statistics.vue
+++ frontend/src/components/Manage/Statistics.vue
@@ -1,18 +1,27 @@
 <template>
   <v-container class="mx-auto px-8">
-		<GeoChart />
+		<v-row>
+			<v-col>
+				<StatSubHeader 
+					title="countries"
+					subtitle="Overall Period"
+				/>
+				<GeoChart />
+			</v-col>
+		</v-row>
   </v-container>
 </template>
 
 <script>
 import GeoChart from '@/components/Manage/GeoChart.vue'
+import StatSubHeader from '@/components/Manage/StatSubHeader.vue'
 export default {
 	name: 'Statistics',
 	props: {
 
 	},
 	components: {
-		GeoChart
+		GeoChart, StatSubHeader
 	}
 }
 </script>
