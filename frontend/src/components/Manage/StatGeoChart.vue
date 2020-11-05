b50bf356fca73757a8cdce2a258946dd92132dde
Cotidie <daily142857@gmail.com>
2020-11-05 23:52:28 +0900

    GeoChart - 반응형
    화면 Breakpoint 변경 시 크기 조정

--- frontend/src/components/Manage/StatGeoChart.vue
+++ frontend/src/components/Manage/StatGeoChart.vue
@@ -5,11 +5,13 @@
 	- data: 적용할 국가별 데이터 (배열)
 -->
 <template>
-	<div class="display-flex flex-row">
-		<ProgressChart :items="countries" />
+	<div class="display-flex flex-row" ref="parent">
+		<div ref="pChart">
+			<ProgressChart :items="countries"/>
+		</div>
 
-		<div class="flex-grow" :id="_uid">
-			
+		<div class="flex-grow">
+			<div :id="_uid" />
 		</div>
 	</div>
 </template>
@@ -22,6 +24,8 @@ export default {
 	name: 'GeoChart',
 	data: () => ({
 		dataHeader: ['Country', 'Clicks'],
+		breakpoint: null,
+		responsiveWidth: '100%',
 	}),
 	computed: {
 		...mapState(['googleMapAPI']),
@@ -41,7 +45,15 @@ export default {
 				res.push([country, countries[country]])
 			}
 
-			return res
+			return google.visualization.arrayToDataTable(res)
+		},
+		options() {
+			return {
+				height: '210',
+				width: this.responsiveWidth,
+				colorAxis: {colors: ['#e1f7fc', '#3773a5']},
+				legend: 'none'
+			}
 		}
 	},
 	props: {
@@ -58,29 +70,40 @@ export default {
 			default: () => ''
 		}
 	},
+	methods: {
+		drawGeoChart() {
+			let chart = new google.visualization.GeoChart(document.getElementById(this._uid))
+
+			chart.draw(this.dataArray, this.options)
+		},
+		redrawChart() {
+			// 화면 사이즈가 변경되면 크기 새로고침
+			const currentBreakpoint = this.$vuetify.breakpoint.name
+
+			if (currentBreakpoint !== this.breakpoint) {
+				this.responsiveWidth = this.$refs.parent.clientWidth - this.$refs.pChart.clientWidth
+				this.drawGeoChart()
+				this.breakpoint = currentBreakpoint
+			}
+		}
+	},
 	created() {
+		// 1. 구글 GeoChart 그리기
 		google.charts.load('current', {
 			'packages':['geochart'],
 			'mapsApiKey': this.googleMapAPI
 		})
+		google.charts.setOnLoadCallback(this.drawGeoChart)
 
-		// 로드 후 적용할 콜백함수
-		const drawRegionsMap = () => {
-			var data = google.visualization.arrayToDataTable(this.dataArray)
-			var options = {
-				height: '210',
-				colorAxis: {colors: ['#e1f7fc', '#3773a5']},
-				legend: 'none'
-			}
-			var chart = new google.visualization.GeoChart(document.getElementById(this._uid))
-
-			chart.draw(data, options)
-		}
-
-		google.charts.setOnLoadCallback(drawRegionsMap)
+		// 2. 현재 Breakpoint 기록 및 변화 시 새로고침
+		this.breakpoint = this.$vuetify.breakpoint.name
+		window.addEventListener('resize', this.redrawChart)
 	},
 	components: {
 		ProgressChart: StatProgressChart,
+	},
+	beforeDestroy() {
+		window.removeEventListener('resize', this.redrawChart)
 	}
 }
 </script>
